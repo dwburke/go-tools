3a7fc6b461014c30366009d1287df7bbbcb5a2ba
Dan Burke <dburke@addictmud.org>
2018-07-14 16:03:18 -0400

    added HomeDir() function

--- tools.go
+++ tools.go
@@ -1,9 +1,29 @@
 package tools
 
-import "log"
+import (
+	"log"
+	"os"
+	"runtime"
+)
 
 func FatalError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func HomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	} else if runtime.GOOS == "linux" {
+		home := os.Getenv("XDG_CONFIG_HOME")
+		if home != "" {
+			return home
+		}
+	}
+	return os.Getenv("HOME")
 }
