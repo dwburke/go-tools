6cd2338e1fffe5bf60291024918ddc621a01b571
Dan Burke <dburke@addictmud.org>
2018-07-22 17:35:54 -0400

    Add gin helper to combine query params with post params

---
+++ gin.go
@@ -0,0 +1,26 @@
+package tools
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AllGinParams(c *gin.Context) gin.Params {
+	var params gin.Params
+
+	for _, p := range c.Params {
+		params = append(params, gin.Param{Key: p.Key, Value: p.Value})
+	}
+
+	req := c.Request
+	req.ParseForm()
+
+	for k, v := range req.PostForm {
+		if len(v) == 0 {
+			continue
+		}
+
+		params = append(params, gin.Param{Key: k, Value: v[0]})
+	}
+
+	return params
+}
